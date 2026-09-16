c35bb622add980e47e8a02595b7fecf4f7e60037
NatasyaLR26 <natasyalutfiyahramadhan@gmail.com>
2026-09-16 12:01:36 +0700

    Tambah Dashboard Teknisi dengan daftar tugas aktif

--- sto-frontend/src/App.jsx
+++ sto-frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import DashboardPimpinan from './pages/DashboardPimpinan';
+import DashboardTeknisi from './pages/DashboardTeknisi';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard-pimpinan" element={<DashboardPimpinan />} />
+        <Route path="/dashboard-teknisi" element={<DashboardTeknisi />} />
       </Routes>
     </BrowserRouter>
   );
